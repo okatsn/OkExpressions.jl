3430e61a5126b45b93e9647fad4e39650d34509a
Documenter.jl <documenter@juliadocs.github.io>
2022-12-09 08:44:41 +0000

    build based on 14ac057

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = OkExpressions","category":"page"},{"location":"#OkExpressions","page":"Home","title":"OkExpressions","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for OkExpressions.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [OkExpressions]","category":"page"}]
+}
